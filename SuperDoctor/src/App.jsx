23c8cf9843f028f7c162e4799fa132ed4a45b8ae
Abdeali Makda <abdealimac@gmail.com>
2026-05-11 15:20:53 +0530

    inital commit

---
+++ SuperDoctor/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SuperDoctorPage from './components/SuperDoctor';
+
+function App() {
+  return (
+    <>
+      <SuperDoctorPage />
+    </>
+  );
+}
+
+export default App;
