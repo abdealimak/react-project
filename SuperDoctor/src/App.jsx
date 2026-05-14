f4bacc9ff2554c1911c419530f97b29177661c06
Abdeali Makda <abdealimac@gmail.com>
2026-05-14 22:55:33 +0530

    initial update

--- SuperDoctor/src/App.jsx
+++ SuperDoctor/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SuperDoctorPage from './sections/Hero'; // Make sure the path matches your folder structure
+import SuperDoctorPage from './landingpage/Hero'; // Make sure the path matches your folder structure
 
 function App() {
   return (
